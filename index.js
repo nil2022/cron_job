d29272f0c9345fd921e60533a10fba49fcd6c4c5
Nilanjan Haldar <nil.haldar@gmail.com>
2025-03-11 07:50:10 +0530

    Refactor cron job implementation and clean up commented code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const express = require("express");
 const logger = require("morgan");
 const app = express();
 const { formatDate } = require("./formatDate");
-const { default: scheduleTask } = require("./utils/cron.util");
 const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,57 +15,57 @@ app.get("/cron", (req, res) => {
   /**************  CRON JOB  ***************/
   const cronSchedule = process.env.CRON_SCHEDULE;
   console.log('cronSchedule: ', cronSchedule);
-  //   cron.schedule(cronSchedule, async () => {
-  //     //RUNS EVERY Specified in .env minute
-  //     const date = new Date();
-  //     const timeNow = formatDate(date);
-  //     const URL_TO_FETCH = process.env.URLTOFETCH;
-  //     const FETCH_OWN = process.env.FETCH_OWN;
+    cron.schedule(cronSchedule, async () => {
+      //RUNS EVERY Specified in .env minute
+      const date = new Date();
+      const timeNow = formatDate(date);
+      const URL_TO_FETCH = process.env.URLTOFETCH;
+      const FETCH_OWN = process.env.FETCH_OWN;
 
-  //     console.time("API Fetched time");
-  //     try {
-  //       let { data, status, statusText, headers, config, request } =
-  //         await axios.get(URL_TO_FETCH);
-  //       await axios.get(FETCH_OWN);
-  //       console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
-  //       console.timeEnd("API Fetched time");
-  //       console.log("API Status using AXIOS:\n", {
-  //         Status: [status, statusText],
-  //       });
-  //     } catch (error) {
-  //       console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
-  //       console.timeEnd("API Fetched time");
-  //       console.log({
-  //         Status: [error.response.status, error.response.statusText],
-  //       });
-  //     }
-  //   });
+      console.time("API Fetched time");
+      try {
+        let { data, status, statusText, headers, config, request } =
+          await axios.get(URL_TO_FETCH);
+        await axios.get(FETCH_OWN);
+        console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
+        console.timeEnd("API Fetched time");
+        console.log("API Status using AXIOS:\n", {
+          Status: [status, statusText],
+        });
+      } catch (error) {
+        console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
+        console.timeEnd("API Fetched time");
+        console.log({
+          Status: [error.response.status, error.response.statusText],
+        });
+      }
+    });
 
-  scheduleTask(cronSchedule, async () => {
-    //RUNS EVERY Specified in .env minute
-    const date = new Date();
-    const timeNow = formatDate(date);
-    const URL_TO_FETCH = process.env.URLTOFETCH;
-    const FETCH_OWN = process.env.FETCH_OWN;
+  // scheduleTask(cronSchedule, async () => {
+  //   //RUNS EVERY Specified in .env minute
+  //   const date = new Date();
+  //   const timeNow = formatDate(date);
+  //   const URL_TO_FETCH = process.env.URLTOFETCH;
+  //   const FETCH_OWN = process.env.FETCH_OWN;
 
-    console.time("API Fetched time");
-    try {
-      let { data, status, statusText, headers, config, request } =
-        await axios.get(URL_TO_FETCH);
-      await axios.get(FETCH_OWN);
-      console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
-      console.timeEnd("API Fetched time");
-      console.log("API Status using AXIOS:\n", {
-        Status: [status, statusText],
-      });
-    } catch (error) {
-      console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
-      console.timeEnd("API Fetched time");
-      console.log({
-        Status: [error.response.status, error.response.statusText],
-      });
-    }
-  });
+  //   console.time("API Fetched time");
+  //   try {
+  //     let { data, status, statusText, headers, config, request } =
+  //       await axios.get(URL_TO_FETCH);
+  //     await axios.get(FETCH_OWN);
+  //     console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
+  //     console.timeEnd("API Fetched time");
+  //     console.log("API Status using AXIOS:\n", {
+  //       Status: [status, statusText],
+  //     });
+  //   } catch (error) {
+  //     console.log("URL:", URL_TO_FETCH, `[Access Time: ${timeNow}]`);
+  //     console.timeEnd("API Fetched time");
+  //     console.log({
+  //       Status: [error.response.status, error.response.statusText],
+  //     });
+  //   }
+  // });
 
   res.send("ok");
 });
